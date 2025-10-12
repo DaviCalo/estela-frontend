d4c36a0dce245c2907ebb668c9b47b1559a333f8
Davi C. Pes. <davicalo2d@gmail.com>
2025-10-12 19:38:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Login from './components/login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Login />
+      </main>
+    </div>
+  );
+}
+// <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//className="App-link"
+//href="https://reactjs.org"
+//target="_blank"
+//rel="noopener noreferrer"
+//        >
+//Learn React
+//        </a>
+//      </header>
+export default App;
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,63 @@
+
+import React, { useState } from 'react';
+import './login.css';
+import logo from '../../images/logo.png';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Dados submetidos:', { email, password });
+  };
+
+  return (
+    <div className="login-container">
+        <section id="logo">
+            <img src={logo} alt="Logo"/>
+        </section>
+        <section id="card">
+            <div id="buttons-choice">
+                <button id="login-button" class="button-stitch button-stitch-selected">Login</button>
+                <button id="register-button" class="button-stitch button-stitch-unselected">Criar conta</button>
+            </div>
+            <div class="form-group">
+                <label for="email" class="form-label">Email:</label>
+                <div class="input-wrapper">
+                    <span class="input-icon">@</span>
+                    <input
+                        type="email"
+                        id="email"
+                        class="form-input"
+                        placeholder="Seu email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                  />
+                </div>
+                <label for="email" class="form-label">Senha:</label>
+                <div class="input-wrapper">
+                    <span class="input-icon">@</span>
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-input"
+                      placeholder="Sua senha"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                </div>
+                <div class="remember-login">
+                    <input type="checkbox" name="remember-login" id="remember-login-checkbox"/>
+                    <label for="remember-login-checkbox" class="text-small-regular">Lembrar</label>
+                </div>
+                <a href="https://secure.nuuvem.com/br-en/account/forgot-password">Esqueci minnha senha</a>
+            </div>
+        </section>
+    </div>
+  );
+};
+
+export default Login;
