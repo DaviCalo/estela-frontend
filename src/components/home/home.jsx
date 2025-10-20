73df8497b89367c8ec72c9ca612cf7d76ac302ea
Nicole A. <nicoleamanda@alu.ufc.br>
2025-10-20 18:54:22 -0300

    Alterations in Login page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,6 +3,7 @@ import './home.css';
 import logo from '../../../src/images/logo.png';
 import Sidebar from '../sidebar/sidebar.jsx';
 import Cardhome from '../card/cardhome.jsx';
+import banner from '../../images/bannerhk.png';
 
 const Home = () => {
     return (
@@ -10,11 +11,15 @@ const Home = () => {
             <div id="full">
                 <Sidebar />
                 <div id="content">
-                    <div id="banner-image"></div>
-                    <div id="cards-wrapper">
+                    <div id="banner-image" style={{ backgroundImage: `url(${banner})` }}>
+                    </div>
+                    <div id="content-wrapper">
                         <h1>Lançamentos</h1>
-                        <p>Veja as novidades mais populares desse mês!</p>
-                        <div id="cards-container">
+                        <p>Veja as novidades populares desse mês</p>
+                        <div id="card-wrapper">
+                            <Cardhome />
+                            <Cardhome />
+                            <Cardhome />
                             <Cardhome />
                             <Cardhome />
                         </div>
